4b3f47476c9b8aca99d4ced08bf3d938ebfdd64f
Matheus Carvalho <henriquematheus.c@gmail.com>
2023-06-20 19:14:52 -0300

    Fix API path

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://ec2-3-15-235-43.us-east-2.compute.amazonaws.com:8080/',
+  baseURL: 'http://ec2-3-15-235-43.us-east-2.compute.amazonaws.com:8080/',
   headers: {
     'Content-Type': 'application/json',
   },
